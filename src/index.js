13170cdd57411794bf174b9580a6fe78dd7fe9a9
aya-yama <103544757+aya-yama@users.noreply.github.com>
2022-04-14 17:08:46 +0900

    タスクの削除機能

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const onClickadd = () => {
   // div生成
   const div = document.createElement("div");
   div.className = "list-row";
-  console.log(div);
 
   // liタグ生成
   const li = document.createElement("li");
@@ -18,19 +17,22 @@ const onClickadd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => alert("完了"));
-  console.log(completeButton);
 
   // button(削除)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
-  deleteButton.addEventListener("click", () => alert("削除"));
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ（div）を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    console.log(deleteTarget);
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
   console.log(deleteButton);
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
-  console.log(div);
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
