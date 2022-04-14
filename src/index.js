461923cd510578aba7aa7d82761d29ceafdf6fb8
aya-yama <103544757+aya-yama@users.noreply.github.com>
2022-04-14 11:21:10 +0900

    タスクの追加機能

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+const onClickadd = () => {
+  // テキストボックスの値を取得し、初期化する。
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-low";
+  console.log(div);
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  console.log(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document.getElementById("add-button").addEventListener("click", () => {
+  onClickadd();
+});
